63cd2432a9ba68f7c7baa05b802596c36511bd47
ChaseTeague <teaguechase@yahoo.com>
2023-09-29 15:21:37 -0400

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const newTaskF = document.getElementById('create-task-form');
+
+  newTaskF.addEventListener('submit', newTask);
 });
+
+const newTask = event => {
+  event.preventDefault();
+  const newTaskDes = document.getElementById('new-task-description');
+  const task = document.createElement('li');
+  task.innerHTML = newTaskDes.value;
+  appendTask(task);
+  event.target.reset();
+};
+
+const appendTask = task => {
+  document.getElementById('tasks').appendChild(task);
+};
